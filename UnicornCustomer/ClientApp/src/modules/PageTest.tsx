88b8ec890330f5d50c84832edfcec5d67d345e2b
haksuman <h.aksuman@gmail.com>
2023-08-19 22:46:39 +0300

    mui installed and tested

--- UnicornCustomer/ClientApp/src/modules/PageTest.tsx
+++ UnicornCustomer/ClientApp/src/modules/PageTest.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Card, CardContent, CardHeader } from "@mui/material/";
 
 const PageTest = () => {
-  return <div>PageTest</div>;
+  return (
+    <Card variant="outlined" className="bg-red">
+      <CardHeader title="Welcome to the administration" />
+      <CardContent>Foo</CardContent>
+    </Card>
+  );
 };
 
 export default PageTest;
