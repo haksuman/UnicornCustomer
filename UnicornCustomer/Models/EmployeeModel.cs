4fcdbe6e3ed00bab5d4daa56221ade390a3294e5
haksuman <h.aksuman@gmail.com>
2023-08-19 19:01:08 +0300

    employee and address model created

---
+++ UnicornCustomer/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System;
+
+namespace UnicornCustomer.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string First { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public AddressModel Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
