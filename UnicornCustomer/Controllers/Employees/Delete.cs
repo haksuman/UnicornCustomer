649403d7282fe3977818ae47077ab2a7831f7e20
haksuman <h.aksuman@gmail.com>
2023-08-19 20:24:49 +0300

    delete employee controller created

---
+++ UnicornCustomer/Controllers/Employees/Delete.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace UnicornCustomer.Controllers.Employees
+{
+    public class Delete : BaseController
+    {
+        [HttpDelete("/api/employees/{id}")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            var employees = ReadEmployeesFromFile();
+            var employee = employees.FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employees.Remove(employee);
+
+            WriteEmployeesToFile(employees);
+
+            return Ok();
+        }
+    }
+}
