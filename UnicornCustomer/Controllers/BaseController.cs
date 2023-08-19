a86733622f7f3477fa0a9a291de15bf057fc9cdf
haksuman <h.aksuman@gmail.com>
2023-08-19 19:34:31 +0300

    base controller for employee added

---
+++ UnicornCustomer/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using UnicornCustomer.Models;
+
+namespace UnicornCustomer.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private readonly string _filePath = "Data/employees.json";
+
+        protected List<EmployeeModel> ReadEmployeesFromFile()
+        {
+            var employees = new List<EmployeeModel>();
+            using (StreamReader r = new StreamReader(_filePath))
+            {
+                string json = r.ReadToEnd();
+                employees = JsonConvert.DeserializeObject<List<EmployeeModel>>(json);
+            }
+            return employees;
+        }
+    }
+}
--- UnicornCustomer/Controllers/Employees/Get.cs
+++ UnicornCustomer/Controllers/Employees/Get.cs
@@ -1,27 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.IO;
+﻿using Microsoft.AspNetCore.Mvc;
 using UnicornCustomer.Models;
 
 namespace UnicornCustomer.Controllers.Employees
 {
     [ApiController]
-    public class Get : ControllerBase
+    public class Get : BaseController
     {
-        private readonly string _filePath = "Data/employees.json";
-
-        private List<EmployeeModel> ReadEmployeesFromFile()
-        {
-            var employees = new List<EmployeeModel>();
-            using (StreamReader r = new StreamReader(_filePath))
-            {
-                string json = r.ReadToEnd();
-                employees = JsonConvert.DeserializeObject<List<EmployeeModel>>(json);
-            }
-            return employees;
-        }
 
         [HttpGet("/api/employees/{id}")]
         public ActionResult<EmployeeModel> GetEmployee(int id)
